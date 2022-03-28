4543fe9ee226c4a256f77f689efbd793f26c0058
Juan Vidal <vidalbagnoli@gmail.com>
2022-03-28 18:13:15 -0300

    Latest Bug Fix

--- src/backend/categories.js
+++ src/backend/categories.js
@@ -5,19 +5,35 @@ import imgCat3 from "../assets/img/lenceria.jpg";
 import imgCat4 from "../assets/img/paletas.png";
 
 export default function getCategory() {
-	return new Promise((resolve, reject) => {
-		const query = getFirestore().collection("categorias");
-		// Pedimos los datos
-		query.get().then((response) => {
-			if (response.size === 0) reject("Empty");
-			const data = response.docs.map((doc) => ({
-				...doc.data(),
-				id: doc.itemID,
-				image: doc.image,
-			}));
+  function switchImage(params) {
+    	console.log("getCategory()::switchImage(params)=", params);
+	  switch (String(params)) { 
+      case "imgCat1":
+        return imgCat1;
+      case "imgCat2":
+        return imgCat2;
+      case "imgCat3":
+        return imgCat3;
+      case "imgCat4":
+        return imgCat4;
+      default:
+        return imgCat4; 
+    }
+  }
 
-			console.log("data",data);
-			resolve(data);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    const query = getFirestore().collection("categorias");
+    // Pedimos los datos
+    query.get().then((response) => {
+      if (response.size === 0) reject("Empty");
+      const data = response.docs.map((doc) => ({
+        ...doc.data("itemID"),
+        //id: doc.data("itemID"),
+        //image: switchImage(doc.data.catID),
+      }));
+
+      console.log("data", data);
+      resolve(data);
+    });
+  });
 }
